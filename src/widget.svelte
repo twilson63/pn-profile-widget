c9297f924e18f169d9215f1b553ca35124ebd0de
Tom Wilson <tom@hyper.io>
2022-06-29 14:59:48 +0000

    added mobile view

--- src/widget.svelte
+++ src/widget.svelte
@@ -26,9 +26,167 @@
   }
 </script>
 
-<div class="h-[900px]">
+<div class="md:hidden h-[400px]">
   <div
-    class="bg-[url('{backgroundUrl}')] bg-cover bg-no-repeat w-full h-[660px] mb-[50px]"
+    class="bg-[url('{backgroundUrl}')] bg-cover bg-no-repeat w-full h-[100px] mb-[10px]"
+  >
+    <figure class="flex justify-center">
+      <img
+        alt="avatar"
+        src={avatarUrl}
+        class="shadow-xl rounded-full border-4 h-[250px] w-[250px] bg-base-300"
+      />
+    </figure>
+    <div class="flex flex-col justify-center ml-8">
+      <p class="m-1 text-3xl tracking-tight text-base-600">{name}</p>
+      <p class="m-1 text-xl text-base-600">{bio}</p>
+    </div>
+    <ul class="w-full flex flex-row-reverse space-x-4 p-5">
+      {#if !isEmpty(facebook)}
+        <li>
+          <a
+            href="https://facebook.com/{facebook}"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button
+              style="
+        background-image: url({icon_repo}/facebook-icon.png);
+      "
+              class="w-[32px] h-[32px] rounded-xl bg-cover bg-no-repeat"
+            />
+          </a>
+        </li>
+      {/if}
+      {#if !isEmpty(linkedin)}
+        <li>
+          <a
+            href="https://www.linkedin.com/in/{linkedin}"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button
+              style="
+            background-image: url({icon_repo}/linkedin-icon.png);
+          "
+              class="w-[32px] h-[32px] rounded-xl bg-cover bg-no-repeat"
+            />
+          </a>
+        </li>
+      {/if}
+      {#if !isEmpty(github)}
+        <li>
+          <a
+            href="https://github.com/{github}"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button
+              style="
+            background-image: url({icon_repo}/github-icon.png);
+          "
+              class="w-[32px] h-[32px] rounded-xl bg-cover bg-no-repeat"
+            />
+          </a>
+        </li>
+      {/if}
+      {#if !isEmpty(discord)}
+        <li>
+          <a
+            href="https://discord.com/users/{discord}"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button
+              style="
+            background-image: url({icon_repo}/discord-icon.png);
+          "
+              class="w-[32px] h-[32px]  rounded-xl bg-cover bg-no-repeat"
+            />
+          </a>
+        </li>
+      {/if}
+      {#if !isEmpty(weavemail)}
+        <li>
+          <button
+            on:click={() => (mailDialog = true)}
+            style="
+          background-image: url({icon_repo}/arweave-mail-icon.png);
+        "
+            class="w-[32px] h-[32px] rounded-xl bg-cover bg-no-repeat"
+          />
+        </li>
+      {/if}
+      {#if !isEmpty(twitch)}
+        <li>
+          <a
+            href="https://twitch.com/{twitch}"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button
+              style="
+            background-image: url({icon_repo}/twitch-icon.png);
+          "
+              class="w-[32px] h-[32px] rounded-xl bg-cover bg-no-repeat"
+            />
+          </a>
+        </li>
+      {/if}
+      {#if !isEmpty(instagram)}
+        <li>
+          <a
+            href="https://instagram.com/{instagram}"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button
+              style="
+            background-image: url({icon_repo}/instagram-icon.png);
+          "
+              class="w-[32px] h-[32px] rounded-xl bg-cover bg-no-repeat"
+            />
+          </a>
+        </li>
+      {/if}
+      {#if !isEmpty(youtube)}
+        <li>
+          <a
+            href="https://youtube.com/c/{youtube}"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button
+              style="
+            background-image: url({icon_repo}/youtube-icon.png);
+          "
+              class="w-[32px] h-[32px] rounded-xl bg-cover bg-no-repeat"
+            />
+          </a>
+        </li>
+      {/if}
+      {#if !isEmpty(twitter)}
+        <li>
+          <a
+            href="https://twitter.com/{twitter}"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button
+              style="
+            background-image: url({icon_repo}/twitter-icon.png);
+          "
+              class="w-[32px] h-[32px] rounded-xl bg-cover bg-no-repeat"
+            />
+          </a>
+        </li>
+      {/if}
+    </ul>
+  </div>
+</div>
+<div class="hidden md:block h-[400px]">
+  <div
+    class="bg-[url('{backgroundUrl}')] bg-cover bg-no-repeat w-full h-[300px] mb-[50px]"
   >
     <div class="w-full h-full flex flex-col justify-between relative">
       <ul class="w-full flex flex-row-reverse p-5">
@@ -172,7 +330,7 @@
           </li>
         {/if}
       </ul>
-      <div class="w-full flex m-5 absolute top-[565px] left-0">
+      <div class="w-full flex m-5 absolute top-[200px] left-0">
         <img
           alt="avatar"
           src={avatarUrl}
